eeda7bcb822731d4519aaac5a9b3b6e716a6fca2
Jesús Meza <jame181185@gmail.com>
2023-10-18 13:58:48 -0700

    Se agrega un poco de seguridad al configurar el CORS.

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [process.env.MYSOFTLINKS_URL],
+      methods: ['GET', 'POST'],
+    }
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe ({
