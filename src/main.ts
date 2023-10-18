5d6bc25597751a1e5345312839a71cc3f7afd195
Jesús Meza <jame181185@gmail.com>
2023-10-18 14:09:52 -0700

    Se modifica cambia la forma de obtener el dominio para el CORS.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: [process.env.MYSOFTLINKS_URL],
+      origin: [process.env.MYSOFTLINKS_URL.replace('/#', '')],
       methods: ['GET', 'POST'],
     }
   });
