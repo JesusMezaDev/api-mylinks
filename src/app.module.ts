f97ee795e701078ee158e2ecfa6e71d2fd760c75
Jesús Meza <jame181185@gmail.com>
2024-10-17 11:50:00 -0700

    Se actualizan paquetes y se quita el contenido estático, todo esto con el fin de evitar vulverabilidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { join } from 'path';
+// import { join } from 'path';
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
+/* Por el momento se quitará la página para evitar vulnerabilidades */
+// import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { LinksModule } from './links/links.module';
@@ -10,9 +11,9 @@ import { LinksModule } from './links/links.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'public'),
+    // }),
     MongooseModule.forRoot(process.env.MONGODB),
     LinksModule
   ],
